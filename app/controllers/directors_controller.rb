68383ddc73bce2af38da9c906241077f9a5a55c2
devashreegoenka <devashree@uchicago.edu>
2024-11-05 22:16:34 +0000

    done

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -3,6 +3,16 @@ class DirectorsController < ApplicationController
     render ({ :template => "director_templates/list"})
   end 
 
+  def youngest
+    @the_youngest_director = Director.where.not(dob: nil).order(dob: :asc).last
+    render ({ :template => "director_templates/youngest"})
+  end 
+
+  def oldest
+    @the_oldest_director = Director.where.not(dob: nil).order(dob: :asc).first
+    render ({ :template => "director_templates/oldest"})
+  end 
+
   def show
     the_id = params.fetch("the_id")
     matching_records = Director.where({ :id => the_id})
